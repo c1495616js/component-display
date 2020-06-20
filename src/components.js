f6e41a102a8e3e8312d2c1fefb79a81f798d9444
Jerry Wang <jerrywang@MacBook-Pro.hitronhub.home>
2020-06-20 13:38:18 -0700

    add item padding

--- src/components.js
+++ src/components.js
@@ -2,4 +2,14 @@ import Login from './components/login';
 
 export default [
   { name: 'login', desc: 'This is Login page', component: Login },
+  { name: 'login2', desc: 'This is Login page', component: Login },
+  { name: 'login3', desc: 'This is Login page', component: Login },
+  { name: 'login4', desc: 'This is Login page', component: Login },
+  { name: 'login5', desc: 'This is Login page', component: Login },
+  { name: 'login6', desc: 'This is Login page', component: Login },
+  { name: 'login7', desc: 'This is Login page', component: Login },
+  { name: 'login8', desc: 'This is Login page', component: Login },
+  { name: 'login9', desc: 'This is Login page', component: Login },
+  { name: 'login91', desc: 'This is Login page', component: Login },
+  { name: 'login92', desc: 'This is Login page', component: Login },
 ];
